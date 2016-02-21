8b576c9e77023305993596de8e5cfb4a04643108
Cameron Hunter <chunter@twitter.com>
2016-02-20 23:57:23 -0800

    Remove files

--- src/components/SkillEditor.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Editor from './Editor';
-import Try from 'promise-try';
-
-Try(() => require('codemirror/mode/javascript/javascript'));
-
-export default props => (
-  <Editor {...props} mode={{ name: 'javascript' }} />
-);
--- src/redux/modules/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { routeReducer } from 'react-router-redux';
-
-export default combineReducers({
-  routing: routeReducer
-});
