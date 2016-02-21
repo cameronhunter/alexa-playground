8bab96772a2f191df1bd28d956909df0128b7122
Cameron Hunter <chunter@twitter.com>
2016-02-21 12:27:50 -0800

    Pull more in vendor

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,20 +1,35 @@
-import webpack from "webpack";
-import path from "path";
+import webpack from 'webpack';
+import path from 'path';
 
 export default {
   entry: {
-    app: path.join(__dirname, "src", "client.js"),
-    vendor: ["react", "react-dom", "react-router", "babel-polyfill"]
+    app: path.join(__dirname, 'src', 'client.js'),
+    vendor: [
+      'alexa-lambda-skill',
+      'babel-core',
+      'babel-plugin-transform-decorators-legacy',
+      'babel-plugin-transform-es2015-modules-commonjs',
+      'babel-polyfill',
+      'babel-preset-es2015',
+      'babel-preset-stage-1',
+      'codemirror/mode/javascript/javascript',
+      'promise-try',
+      'react',
+      'react-code-mirror',
+      'react-dom',
+      'react-redux',
+      'react-router'
+    ]
   },
   output: {
-    path: path.join(__dirname, "static", "dist"),
-    filename: "[name].js",
-    publicPath: "/dist"
+    path: path.join(__dirname, 'static', 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist'
   },
   module: {
     loaders: [
-      { test: /\.json$/, loader: "json-loader" },
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader", query: { cacheDirectory: true } }
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', query: { cacheDirectory: true } }
     ]
   },
   plugins: [
@@ -23,7 +38,7 @@ export default {
     new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
+      'process.env.NODE_ENV': `'${process.env.NODE_ENV}'`
     })
   ],
   node: {
@@ -31,4 +46,4 @@ export default {
     module: 'empty',
     net: 'empty'
   }
-}
+};
