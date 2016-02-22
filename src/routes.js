08ccef8cd8d145cd4d0335f2c8ded5630f4dc2c0
Cameron Hunter <chunter@twitter.com>
2016-02-21 20:20:50 -0800

    Accept all routes

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,5 @@ import { Route, IndexRoute } from 'react-router';
 import Playground from './containers/Playground';
 
 export default (
-  <Route path="/">
-    <IndexRoute component={Playground} />
-  </Route>
+  <Route path="*" component={Playground} />
 );
