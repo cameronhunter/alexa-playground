bfa6762dbeb4867c51b6376fb3203e87443a1f6d
Cameron Hunter <chunter@twitter.com>
2016-03-30 23:09:44 -0700

    Updating dependencies

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,11 +1,8 @@
-import { syncHistory } from 'react-router-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import reducer from './modules';
 
 export default (history) => createStore(
   reducer,
-  applyMiddleware(
-    syncHistory(history)
-  )
+  applyMiddleware()
 );
